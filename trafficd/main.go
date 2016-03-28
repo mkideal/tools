088489bd3b0bb19894df8c47160a4bc09e73a493
wangshijin <mkideal2@gmail.com>
2016-03-28 21:00:32 +0800

    revert trafficd

--- trafficd/main.go
+++ trafficd/main.go
@@ -10,28 +10,22 @@ import (
 type argT struct {
 	cli.Helper
 	cli.Addr
-	Maps map[string]uint16 `cli:"M" usage:"addr mapping"`
-}
-
-func (argv *argT) Validate(ctx *cli.Context) error {
-	//TODO: validate something or remove this function.
-	return nil
+	Maps map[string]string `cli:"M" usage:"addr mapping"`
 }
 
 func run(ctx *cli.Context, argv *argT) error {
 	if argv.Maps == nil {
-		argv.Maps = make(map[string]uint16)
+		argv.Maps = make(map[string]string)
 	}
 	ctx.JSONIndentln(argv.Maps, "", "    ")
 	addr := fmt.Sprintf("%s:%d", argv.Host, argv.Port)
 	return http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		ctx.String("req: %v", *req)
-		port, ok := argv.Maps[req.Host]
+		readdr, ok := argv.Maps[req.Host]
 		if !ok {
-			fmt.Fprintf(w, "could not redirect %q to new address", req.Host)
+			fmt.Fprintf(w, "could not redirect %q to other address", req.Host)
 			return
 		}
-		urlStr := fmt.Sprintf("%s://%s:%d%s?%s", "http", req.Host, port, req.URL.Path, req.URL.RawQuery)
+		urlStr := fmt.Sprintf("%s://%s%s?%s", "http", readdr, req.URL.Path, req.URL.RawQuery)
 		ctx.String("redirect from %q to %q\n", req.URL.Path, urlStr)
 		http.Redirect(w, req, urlStr, http.StatusUseProxy)
 	}))
